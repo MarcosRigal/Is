3056861f5f442fda28aa54973e7da463e49e32a9
moruno21 <amoruno21@gmail.com>
2020-12-05 18:02:44 +0100

    funciones clase sendero

--- P4/GestorDeParques/claseSendero/sendero.h
+++ P4/GestorDeParques/claseSendero/sendero.h
@@ -7,35 +7,35 @@ using namespace std;
 class Sendero{
     private:
 
-    int difficultyLevel_;
-    string name_;
-    int parkID_;
-    int trailID_;
-    bool trailStatus_;
+        int difficultyLevel_;
+        string name_;
+        int parkID_;
+        int trailID_;
+        bool trailStatus_;
 
     public:
 
-    Sendero(int parkID, int trailID){
-        parkID_ = parkID;
-        trailID_ = trailID;
-    }
+        Sendero(int parkID, int trailID){
+            parkID_ = parkID;
+            trailID_ = trailID;
+        }
 
-    inline int getDifficultyLevel() const { return difficultyLevel_; }
-    inline string getName() const { return name_; }
-    inline int getParkID() const { return parkID_; }
-    inline int getTrailID() const { return trailID_; }
-    inline bool getTrailStatus() const { return trailStatus_; }
+        inline int getDifficultyLevel() const { return difficultyLevel_; }
+        inline string getName() const { return name_; }
+        inline int getParkID() const { return parkID_; }
+        inline int getTrailID() const { return trailID_; }
+        inline bool getTrailStatus() const { return trailStatus_; }
 
-    bool setDifficultyLevel(int const difficultyLevel);
-    inline bool setName(string const name) { name_ = name; }
+        bool setDifficultyLevel(int const difficultyLevel);
+        inline bool setName(string const name) { name_ = name; }
 
-    /* Para implementarla necesito tener hecha la clase parque
-    bool setParkID(int const parkID); */
+        /* Para implementarla necesito tener hecha la clase parque
+        bool setParkID(int const parkID); */
 
-    /*Para implementarla necesito tener el vector/lista de senderos
-    bool setTrailID(int const trailID); */
-    
-    inline bool setTrailStatus(bool const trailStatus) { trailStatus_ = trailStatus; }
+        /*Para implementarla necesito tener el vector/lista de senderos
+        bool setTrailID(int const trailID); */
+        
+        inline bool setTrailStatus(bool const trailStatus) { trailStatus_ = trailStatus; }
 
 };
 #endif
