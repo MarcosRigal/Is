947ae0de21afd5d7404585ea9b0f77122aade145
moruno21 <amoruno21@gmail.com>
2020-12-05 17:55:13 +0100

    funciones clase sendero

--- P4/GestorDeParques/claseSendero/sendero.cc
+++ P4/GestorDeParques/claseSendero/sendero.cc
@@ -1,1 +1,10 @@
 #include "sendero.h"
+
+bool Sendero::setDifficultyLevel(int const difficultyLevel){
+    if (difficultyLevel<1 || difficultyLevel>10){
+        return false;
+    }
+
+    difficultyLevel_ = difficultyLevel;
+    return true;
+}
--- P4/GestorDeParques/claseSendero/sendero.h
+++ P4/GestorDeParques/claseSendero/sendero.h
@@ -20,17 +20,22 @@ class Sendero{
         trailID_ = trailID;
     }
 
-    inline int getDifficultyLevel() const { return difficultyLevel_; };
-    inline string getName() const { return name_; };
-    inline int getParkID() const { return parkID_; } ;
-    inline int getTrailID() const { return trailID_; };
-    inline bool getTrailStatus() const { return trailStatus_ };
+    inline int getDifficultyLevel() const { return difficultyLevel_; }
+    inline string getName() const { return name_; }
+    inline int getParkID() const { return parkID_; }
+    inline int getTrailID() const { return trailID_; }
+    inline bool getTrailStatus() const { return trailStatus_; }
 
     bool setDifficultyLevel(int const difficultyLevel);
-    bool setName(string const name);
-    bool setParkID(int const parkID);
-    bool setTrailID(int const trailID);
-    bool setTrailStatus(bool const trailStatus);
+    inline bool setName(string const name) { name_ = name; }
+
+    /* Para implementarla necesito tener hecha la clase parque
+    bool setParkID(int const parkID); */
+
+    /*Para implementarla necesito tener el vector/lista de senderos
+    bool setTrailID(int const trailID); */
+    
+    inline bool setTrailStatus(bool const trailStatus) { trailStatus_ = trailStatus; }
 
 };
 #endif
