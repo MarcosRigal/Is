e8c0f7d1703c4eb630b967bf8d93e667623d516a
moruno21 <amoruno21@gmail.com>
2020-12-07 12:45:49 +0100

    funciones clase trabajador

--- P4/GestorDeParques/claseTrabajador/trabajador.h
+++ P4/GestorDeParques/claseTrabajador/trabajador.h
@@ -8,12 +8,23 @@ using namespace std;
 class Trabajador{
     private:
 
-    string email_;
-    string address;
+        string email_;
+        string address_;
 
     public:
 
-    //Operaciones y funciones de la clase
+        Trabajador(Persona persona);
+
+        string getAddress() const { return address_; }
+        string getEmail() const { return email_; }
+
+        bool setAddress(string address) { address_ = address; }
+        bool setEmail(string email) { email_ = email; }
+
+        bool addRoute(int parkID);
+        bool editRoute(int parkID, int routeID);
+        bool deleteRoute(int parkID, int routeID);
+        bool viewRoute(int parkID, int routeID);
 
 };
 #endif
