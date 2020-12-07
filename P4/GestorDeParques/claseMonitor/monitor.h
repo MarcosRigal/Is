db6928ea5576aacb5d65d73e42502c764d865161
moruno21 <amoruno21@gmail.com>
2020-12-07 20:45:40 +0100

    funciones clase monitor

--- P4/GestorDeParques/claseMonitor/monitor.h
+++ P4/GestorDeParques/claseMonitor/monitor.h
@@ -1,18 +1,33 @@
 #ifndef MONITOR_H
 #define MONITOR_H 
 #include <iostream>
+#include <trabajador.h>
 using namespace std;
 
 class Monitor{
     private:
 
-    int phoneNumber_;
-    float workedHours_;
-    bool status_;
+        int phoneNumber_;
+        float workedHours_;
+        bool status_;
 
     public:
 
-    //Operaciones y funciones de la clase
+        Monitor(int phoneNumber, Trabajador trabajador);
+        
+        int getPhoneNumber() const { return phoneNumber_; }
+        float getWorkedHours() const { return workedHours_; }
+        bool getStatus() const { return status_; }
+
+        /* Los sets deberian ser void????? */
+
+        bool setPhoneNumber(int phoneNumber);
+        bool setWorkedHours(float workedHours);
+        bool setStatus(bool status);
+
+        bool notifyIncidentOnRoute(int parkID, int routeID, string incidence);
+        void getRouteReport(int parkID, int routeID);
+
 
 };
 #endif
