778b4f14f84329f58f99a4314e1b8754bc5b5507
MarcosRigal <riveragavilanmarcos@gmail.com>
2020-12-08 14:52:12 +0100

    Done class Administrativo

--- P4/GestorDeParques/claseAdministrativo/administrativo.h
+++ P4/GestorDeParques/claseAdministrativo/administrativo.h
@@ -20,7 +20,23 @@ class Administrativo : public Trabajador
     bool deleteTrail(list<Parque> &listOfParks);
     void viewTrail(list<Parque> &listOfParks);
 
-    
+    bool addMonitor(list<Monitor> &listOfMonitors);
+    bool editMonitor(list<Monitor> &listOfMonitors);
+    bool deleteMonitor(list<Monitor> &listOfMonitors);
+    void viewMonitor(list<Monitor> &listOfMonitors);
+
+    bool addAdministrator(list<Administrativo> &listOfAdministrators);
+    bool editAdministrator(list<Administrativo> &listOfAdministrators);
+    bool deleteAdministrator(list<Administrativo> &listOfAdministrators);
+    void viewAdministrator(list<Administrativo> &listOfAdministrators);
+
+    bool addExcursionist(list<Senderista> &listOfExcursionists);
+    bool editExcursionist(list<Senderista> &listOfExcursionists);
+    bool deleteExcursionist(list<Senderista> &listOfExcursionists);
+    void viewExcursionist(list<Senderista> &listOfExcursionists);
+
+    bool addExcursionistToRoute(list<Parque> &listOfParks, list<Senderista> &listOfExcursionists);
+    bool deleteExcursionistFromRoute(list<Parque> &listOfParks, list<Senderista> &listOfExcursionists);    
 
 };
 #endif
