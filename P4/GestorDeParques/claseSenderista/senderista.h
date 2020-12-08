3faebe1e0956a6807f321b7af83a12f3f34dc1ef
MarcosRigal <riveragavilanmarcos@gmail.com>
2020-12-08 13:40:10 +0100

    Done Senderist

--- P4/GestorDeParques/claseSenderista/senderista.h
+++ P4/GestorDeParques/claseSenderista/senderista.h
@@ -3,18 +3,27 @@
 #include <iostream>
 #include <string>
 #include <list>
+#include "../clasePersona/persona.h"
+#include "../claseRuta/ruta.h"
 using namespace std;
 
-class Senderista{
+class Senderista : public Persona
+{
     private:
 
-    list participatesOnRoute_;
+    list<Ruta> participatesOnRoute_;
     int phoneNumber_;
     string specialRequirements_;
 
     public:
 
-    //Operaciones y funciones de la clase
+    Senderista(string dni, int phoneNumber) : Persona(dni) {phoneNumber_=phoneNumber;}
+    inline list<Ruta> getParticipatesOnRoute() const {return participatesOnRoute_;}
+    inline int getPhoneNumber() const {return phoneNumber_;}
+    inline string getSpecialRequirements() const {return specialRequirements_;}
+    inline bool setParticipatesOnRoute(list<Ruta> const &participatesOnRute){if(participatesOnRute.empty()){return false;}participatesOnRoute_=participatesOnRute; return true;}
+    inline bool setPhoneNumber(int const &phoneNumber){if(phoneNumber<100000000){return false;}phoneNumber_=phoneNumber; return true;}
+    inline bool setSpecialRequirements(string const &specialRequirements){if(specialRequirements.empty()){return false;}specialRequirements_=specialRequirements; return true;}
 
 };
 #endif
