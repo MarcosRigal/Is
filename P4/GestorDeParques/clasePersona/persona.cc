4aa262ad8acb46005e1a12181eb37048158656aa
moruno21 <amoruno21@gmail.com>
2020-12-19 23:26:08 +0100

    borrado funcion dniIsValid

--- P4/GestorDeParques/clasePersona/persona.cc
+++ P4/GestorDeParques/clasePersona/persona.cc
@@ -1,27 +1,6 @@
 #include "../systemFunctions/systemFunctions.h"
 #include "persona.h"
 
-bool dniIsValid(string dni){
-	if (dni.size()!=9){
-		return false;                   //Error si el dni introducido no tiene 9 caracteres
-	}
-
-	for (int i=0; i<8; i++){
-		if (!isdigit(dni[i])){
-			return false;               //Error si uno o varios de los 8 primeros elementos del dni introducido no son digitos
-		}
-	}
-
-	dni[8]=toupper(dni[8]);             //Pasamos la letra del dni introducido a mayuscula
-	int numero=stoi(dni);               //Casting a int
-	char letras[]= "TRWAGMYFPDXBNJZSQVHLCKE";
-	if (dni[8]!=letras[(numero%23)]){   //Formulas para comprobar si la letra se corresponde con los 8 digitos del dni introducido
-		return false;                   //Error si la letra del dni introducido no se corresponde con sus 8 digitos
-	}
-	
-	return true;                        //Se finalizara obteniendo que el dni introducido es correcto siempre que no se entre en ninguno de los if anteriores
-}
-
 bool Persona::setDni(string const &dni) { 
     if (dniIsValid(dni)){
         dni_ = dni;
