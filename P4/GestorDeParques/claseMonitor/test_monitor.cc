7f2dd765703a5b43a0847f90541d924cbcfa53b1
moruno21 <amoruno21@gmail.com>
2020-12-21 19:55:05 +0100

    Test notifyIncidentOnRoute

--- P4/GestorDeParques/claseMonitor/test_monitor.cc
+++ P4/GestorDeParques/claseMonitor/test_monitor.cc
@@ -14,31 +14,29 @@ TEST(Monitor, notifyIncidentOnRoute) {
   string email = "correo@gmail.com";
   string address = "calle adelfa nº1";
 
-  Monitor m(dni, email, address, 611222333);
+  Monitor m(dni, email, address, 611222333);      //Creamos un monitor con los datos de arriba
 
-  list<Ruta*> listOfRoutes;
-  list<int> traversedTrails = {1,2,3,4,5};
-  Ruta r(dni, 1, 1, traversedTrails, 30);
-  listOfRoutes.push_back(&r);
+  list<Ruta*> listOfRoutes;                       //Creamos una lista de rutas
+  list<int> traversedTrails = {1,2,3,4,5};        //Creamos una lista de senderos atravesados para poder crear la ruta
+  Ruta r(dni, 1, 1, traversedTrails, 30);         //Creamos la ruta
+  listOfRoutes.push_back(&r);                     //Añadimos la ruta a la lista de rutas
 
-  list<string> listOfIncidences;
 
   string incidence1 = "Ha habido un desprendimiento";
-  listOfIncidences.push_back(incidence1);
+  m.notifyIncidentOnRoute(listOfRoutes, incidence1, 1, 1);  //Añadimos la incidencia 1 a la ruta
 
-  string incidence2 = "Un niño ha sufrido una caida aparatosa y hemos tenido que acudir a urgencias";
-  listOfIncidences.push_back(incidence2);
+  string incidence2 = "Un niño ha sufrido una caida";
+  m.notifyIncidentOnRoute(listOfRoutes, incidence2, 1, 1);  //Añadimos la incidencia 2 a la ruta
   
   string incidence3 = "Una señora mayor se ha desmayado";
-  listOfIncidences.push_back(incidence1);
+  m.notifyIncidentOnRoute(listOfRoutes, incidence3, 1, 1);  //Añadimos la incidencia 3 a la ruta
 
-  m.notifyIncidentOnRoute(listOfRoutes, incidence1, 1, 1);
-  m.notifyIncidentOnRoute(listOfRoutes, incidence2, 1, 1);
-  m.notifyIncidentOnRoute(listOfRoutes, incidence3, 1, 1);
 
-  EXPECT_FALSE(r.getIncidences().empty());
-  EXPECT_EQ(r.getIncidences().size(), 3);
-  EXPECT_EQ(r.getIncidences(), listOfIncidences);
+  EXPECT_FALSE(r.getIncidences().empty());                  //Comprobamos que la lista no está vacía
+  EXPECT_EQ(r.getIncidences().size(), 3);                   //Comprobamos que el tamaño de la lista de incidencias sea 3
+  EXPECT_EQ(r.getIncidences().begin(), incidence1);         //Comprobamos que la incidencia 1 es la primera
+  EXPECT_EQ(r.getIncidences().end(), incidence3);           //Comprobamos que la incidencia 3 es la última
+
 
 
 }
